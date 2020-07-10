cbdad7816004ba2dec89fe89f129804f3db9b74f
pgatsby <gtrgatz@gmail.com>
2020-07-09 21:01:56 -0700

    uploading starter files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+import {ReactComponent as Logo} from "./assets/eaglerocketry-icon.svg"
+
+import "bootstrap/dist/css/bootstrap.css";
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">
+          <Logo /> EagleRocketry
         </a>
-      </header>
-    </div>
-  );
+      </nav>
+    );
+  }
 }
 
 export default App;

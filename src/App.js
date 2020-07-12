3069269d7d480d53e2ff2f960cc65a6ecfbc62cc
pgatsby <gtrgatz@gmail.com>
2020-07-11 20:33:28 -0700

    added React Router and React Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.css";
 
-import Header from './components/header/header.component'
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
+
+import HomePage from "./pages/homepage/homepage.page";
+import SponsorsPage from "./pages/sponsors/sponsors.page";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/sponsors" component={SponsorsPage} />
+        </Switch>
       </div>
     );
   }
---
+++ src/pages/sponsors/sponsors.page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./sponsors.style.scss";
+
+const SponsorsPage = () => (
+    <div className="sponsorspage">
+        <h1>hello world</h1>
+    </div>
+)
+
+export default SponsorsPage;

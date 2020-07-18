4f6c8d3d62222f3645678b133363aa5e2cc83cc3
joaquinrobles64 <joaquinrobles64@yahoo.com>
2020-07-18 00:25:52 -0700

    first branch idk

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,17 +6,17 @@ import Nav from "react-bootstrap/Nav";
 import "./header.style.scss";
 
 const Header = () => (
-  <Navbar collapseOnSelect expand="md" bg="light" fixed="top" >
+  <Navbar collapseOnSelect expand="md" bg="light" fixed="top">
     <Navbar.Brand className="mr-auto">
       <Logo className="logo" />
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="pl-md-4">
-        <Nav.Link href="#OUTREACH">HOME</Nav.Link>
-        <Nav.Link href="#TEAM">OUTREACH</Nav.Link>
-        <Nav.Link href="#SPONSORS">TEAM</Nav.Link>
-        <Nav.Link href="#CONTACT US">SPONSORS</Nav.Link>
+        <Nav.Link href="/">HOME</Nav.Link>
+        <Nav.Link href="/outreach">OUTREACH</Nav.Link>
+        <Nav.Link href="/team">TEAM</Nav.Link>
+        <Nav.Link href="/sponsors">SPONSORS</Nav.Link>
       </Nav>
       <Nav className="ml-auto">
         <Nav.Link href="#SIGN IN">SIGN IN</Nav.Link>

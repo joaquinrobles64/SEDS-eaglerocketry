6c29438f4fe2d50b2942cf44beec2d12a4cd460d
pgatsby <gtrgatz@gmail.com>
2020-07-28 20:55:26 -0700

    + sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+import FormInput from "../../components/form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component"
+import "./sign-in.style.scss";
+
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -10,7 +14,33 @@ class SignIn extends React.Component {
     };
   }
   render() {
-    return <div className="sign-in">hello world</div>;
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign In with your email and password</span>
+        <form onSubmit="">
+          <FormInput
+            type="email"
+            name="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="email"
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="password"
+            required
+          />
+          <div className="buttons">
+            <CustomButton type="submit" inverted>SIGN IN</CustomButton>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
